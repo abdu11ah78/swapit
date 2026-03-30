6599975ec036577c5d81fc7b79e73d534db8735f
Muhammad Abdullah <mjhdha201@gmail.com>
2026-03-30 19:23:27 +0500

    feat: initialize project structure and database migrations

--- backend/src/Infrastructure/DependencyInjection.cs
+++ backend/src/Infrastructure/DependencyInjection.cs
@@ -12,7 +12,7 @@ public static class DependencyInjection
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
         var connectionString = configuration.GetConnectionString("DefaultConnection")
-            ?? "Server=(localdb)\\MSSQLLocalDB;Database=SwapItDb;Trusted_Connection=True;TrustServerCertificate=True;";
+            ?? "Server=DESKTOP-O2OTSGA\\SQLEXPRESS;Database=SwapItDb;Trusted_Connection=True;TrustServerCertificate=True;";
 
         services.AddDbContext<ApplicationDbContext>(options =>
             options.UseSqlServer(connectionString));
